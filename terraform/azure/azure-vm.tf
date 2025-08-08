cbc5984a0473dd95252c02e757b5a769ac12899e
smuruhesan <108723237+smuruhesan@users.noreply.github.com>
2025-08-08 17:58:50 +0800

    Update azure-vm.tf - Renamed resources to CCLABTEST

--- terraform/azure/azure-vm.tf
+++ terraform/azure/azure-vm.tf
@@ -1,12 +1,10 @@
-# main.tf - Terraform code to create a flawed Azure VM for code scanning demonstration.
+# main.tf - Terraform code to create a cclabtest Azure VM for demonstration.
 
 # This provider block configures the Azure provider.
 # It will now automatically read the subscription ID from the environment.
 provider "azurerm" {
   features {}
 }
-
-
 
 # -------------------------------------------------------------------------------------
 # Intentionally Flawed Code for Demonstration Purposes
@@ -31,87 +29,87 @@ variable "admin_password" {
 # Flaw 2: Overly permissive Network Security Group (NSG) rule.
 # This rule allows all inbound traffic from any source to any port.
 # This is a major security flaw and exposes the VM to the public internet without restriction.
-resource "azurerm_network_security_group" "flawed_nsg" {
-  name                = "${var.github_username}-flawed-nsg"
+resource "azurerm_network_security_group" "cclabtest_nsg" {
+  name                = "${var.github_username}-cclabtest-nsg"
   location            = "eastus"
-  resource_group_name = azurerm_resource_group.flawed_rg.name
+  resource_group_name = azurerm_resource_group.cclabtest_rg.name
 
   security_rule {
-    name                       = "allow_all_inbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    name                         = "allow_all_inbound"
+    priority                     = 100
+    direction                    = "Inbound"
+    access                       = "Allow"
+    protocol                     = "*"
+    source_port_range            = "*"
+    destination_port_range       = "*"
+    source_address_prefix        = "*"
+    destination_address_prefix   = "*"
   }
 }
 
 # Resource group for the VM.
-resource "azurerm_resource_group" "flawed_rg" {
-  name     = "${var.github_username}-flawed-rg"
+resource "azurerm_resource_group" "cclabtest_rg" {
+  name     = "${var.github_username}-cclabtest-rg"
   location = "eastus"
 }
 
 # Virtual network for the VM.
-resource "azurerm_virtual_network" "flawed_vnet" {
-  name                = "${var.github_username}-flawed-vnet"
+resource "azurerm_virtual_network" "cclabtest_vnet" {
+  name                = "${var.github_username}-cclabtest-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.flawed_rg.location
-  resource_group_name = azurerm_resource_group.flawed_rg.name
+  location            = azurerm_resource_group.cclabtest_rg.location
+  resource_group_name = azurerm_resource_group.cclabtest_rg.name
 }
 
 # Subnet for the VM.
-resource "azurerm_subnet" "flawed_subnet" {
-  name                 = "${var.github_username}-flawed-subnet"
-  resource_group_name  = azurerm_resource_group.flawed_rg.name
-  virtual_network_name = azurerm_virtual_network.flawed_vnet.name
+resource "azurerm_subnet" "cclabtest_subnet" {
+  name                 = "${var.github_username}-cclabtest-subnet"
+  resource_group_name  = azurerm_resource_group.cclabtest_rg.name
+  virtual_network_name = azurerm_virtual_network.cclabtest_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Public IP address for the VM.
-resource "azurerm_public_ip" "flawed_public_ip" {
-  name                = "${var.github_username}-flawed-public-ip"
-  location            = azurerm_resource_group.flawed_rg.location
-  resource_group_name = azurerm_resource_group.flawed_rg.name
+resource "azurerm_public_ip" "cclabtest_public_ip" {
+  name                = "${var.github_username}-cclabtest-public-ip"
+  location            = azurerm_resource_group.cclabtest_rg.location
+  resource_group_name = azurerm_resource_group.cclabtest_rg.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
 
-# Network interface for the VM. The NSG association will be a separate resource.
-resource "azurerm_network_interface" "flawed_nic" {
-  name                = "${var.github_username}-flawed-nic"
-  location            = azurerm_resource_group.flawed_rg.location
-  resource_group_name = azurerm_resource_group.flawed_rg.name
+# Network interface for the VM.
+resource "azurerm_network_interface" "cclabtest_nic" {
+  name                = "${var.github_username}-cclabtest-nic"
+  location            = azurerm_resource_group.cclabtest_rg.location
+  resource_group_name = azurerm_resource_group.cclabtest_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.flawed_subnet.id
+    subnet_id                     = azurerm_subnet.cclabtest_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.flawed_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.cclabtest_public_ip.id
   }
 }
 
-# CORRECTED: New resource to associate the NSG with the NIC.
-resource "azurerm_network_interface_security_group_association" "flawed_nsg_association" {
-  network_interface_id      = azurerm_network_interface.flawed_nic.id
-  network_security_group_id = azurerm_network_security_group.flawed_nsg.id
+# New resource to associate the NSG with the NIC.
+resource "azurerm_network_interface_security_group_association" "cclabtest_nsg_association" {
+  network_interface_id        = azurerm_network_interface.cclabtest_nic.id
+  network_security_group_id   = azurerm_network_security_group.cclabtest_nsg.id
 }
 
 # Flaw 3: Using a known insecure or outdated VM image.
-resource "azurerm_linux_virtual_machine" "flawed_vm" {
-  name                = "${var.github_username}-flawed-vm"
-  resource_group_name = azurerm_resource_group.flawed_rg.name
-  location            = azurerm_resource_group.flawed_rg.location
+resource "azurerm_linux_virtual_machine" "cclabtest_vm" {
+  name                = "${var.github_username}-cclabtest-vm"
+  resource_group_name = azurerm_resource_group.cclabtest_rg.name
+  location            = azurerm_resource_group.cclabtest_rg.location
   size                = "Standard_DS1_v2"
   admin_username      = "vmadmin"
   admin_password      = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
-    azurerm_network_interface.flawed_nic.id,
+    azurerm_network_interface.cclabtest_nic.id,
   ]
 
   os_disk {
@@ -132,5 +130,5 @@ resource "azurerm_linux_virtual_machine" "flawed_vm" {
 # -------------------------------------------------------------------------------------
 
 output "public_ip_address" {
-  value = azurerm_public_ip.flawed_public_ip.ip_address
+  value = azurerm_public_ip.cclabtest_public_ip.ip_address
 }
